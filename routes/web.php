0155837d33585ee73da7cd3d51d7524f44822090
gure-y <y.8kp.gm@gmail.com>
2020-11-05 00:14:23 +0900

    laravelのルーティングを編集

--- routes/web.php
+++ routes/web.php
@@ -13,9 +13,10 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
+Route::get('/{any}', function () {
     return view('welcome');
-});
+})->where('any', '.*'); // /以下のとこにアクセスしても、welcome.blade.phpを表示させる
+
 
 Auth::routes();
 
